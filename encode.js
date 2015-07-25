08e2e92e71bd3240d61c2ca148cb433fe4c49eb3
Eduardo Bouças <mail@eduardoboucas.com>
2015-07-25 17:09:55 +0100

    Add optional parameter for array mode

--- encode.js
+++ encode.js
@@ -37,9 +37,11 @@ var stringifyPrimitive = function(v) {
   }
 };
 
-module.exports = function(obj, sep, eq, name) {
+module.exports = function(obj, sep, eq, name, arrMode) {
   sep = sep || '&';
   eq = eq || '=';
+  arrMode = arrMode || false;
+  
   if (obj === null) {
     obj = undefined;
   }
@@ -48,7 +50,10 @@ module.exports = function(obj, sep, eq, name) {
     return Object.keys(obj).map(function(k) {
       var ks = encodeURIComponent(stringifyPrimitive(k)) + eq;
       if (Array.isArray(obj[k])) {
-        ks = encodeURIComponent(stringifyPrimitive(k)) + '[]' + eq;
+        if (arrMode) {
+          ks = encodeURIComponent(stringifyPrimitive(k)) + '[]' + eq;          
+        }
+
         return obj[k].map(function(v) {
           return ks + encodeURIComponent(stringifyPrimitive(v));
         }).join(sep);
